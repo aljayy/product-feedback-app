6691d8e0b5fc5b6a5d06ae91b683e32107d1a8da
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-04-28 16:55:15 -0700

    feat: add rotation for chevron icon

--- src/components/SuggestionBar/SuggestionBar.jsx
+++ src/components/SuggestionBar/SuggestionBar.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import chevron from "../../assets/shared/chevronlight.svg";
 
 function SuggestionBar() {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  function toggleDropdown() {
+    setShowDropdown(!showDropdown);
+  }
+
+  console.log(showDropdown);
+
   return (
     <div className="bg-navySecondary h-[5.6rem] px-6 py-2 flex items-center">
       <div className="text-white text-s-body flex gap-2 items-center">
         <p className="font-normal">
-          Sort by : <span className="font-bold">Most Upvotes</span>
+          Sort by :{" "}
+          <span className="font-bold" onClick={toggleDropdown}>
+            Most Upvotes
+          </span>
         </p>
-        <div>
+        <div
+          className={`${
+            showDropdown ? "rotate-180" : "rotate-0"
+          } ease-in-out duration-500`}
+        >
           <img src={chevron} alt="Chevron Icon" />
         </div>
       </div>
