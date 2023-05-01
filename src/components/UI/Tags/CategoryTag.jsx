d2b19cd0e9709adee8b2e9666ff14a5c96f54796
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-01 14:15:01 -0700

    feat: add onClick event handler

--- src/components/UI/Tags/CategoryTag.jsx
+++ src/components/UI/Tags/CategoryTag.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-function CategoryTag({ title, selected }) {
+function CategoryTag({ title, selected, onClick }) {
   return (
     <button
+      onClick={() => onClick(title)}
       className={`${
-        selected ? "bg-blue text-white" : "bg-grey text-blue"
-      } rounded-[1rem] h-7.5 pt-[0.5rem] pb-1.5 px-4 text-s-body cursor-pointer hover:bg-tagHover`}
+        selected ? "bg-blue text-white" : "bg-grey text-blue hover:bg-tagHover"
+      } rounded-[1rem] h-7.5 pt-[0.5rem] pb-1.5 px-4 text-s-body cursor-pointer`}
     >
       {title}
     </button>
