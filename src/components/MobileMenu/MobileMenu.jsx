ad09817f5eee1033474b32a77d667bce50bac276
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-01 15:58:17 -0700

    feat: add RoadmapCard to MobileMenu component

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import CategoryCard from "../Cards/CategoryCard";
+import RoadmapCard from "../Cards/RoadmapCard";
 
 function MobileMenu() {
   return (
     <div className="w-screen right-0 h-full bg-black/[0.5] absolute top-[7.2rem] z-10">
-      <div className="bg-lightGrey w-[27.1rem] h-screen absolute right-0 px-6 pt-6">
+      <div className="bg-lightGrey w-[27.1rem] h-screen absolute right-0 px-6 pt-6 ">
         <CategoryCard />
+        <RoadmapCard />
       </div>
     </div>
   );
