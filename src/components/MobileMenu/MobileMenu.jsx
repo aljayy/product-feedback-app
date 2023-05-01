912c4522049ba723bd603d4179b2d405b0dd1438
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-01 13:44:26 -0700

    feat: add MobileMenu component

---
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CategoryCard from "../Cards/CategoryCard";
+
+function MobileMenu() {
+  return (
+    <div className="w-screen right-0 h-full bg-black/[0.5] absolute top-[7.2rem] z-10">
+      <div className="bg-lightGrey w-[27.1rem] h-screen absolute right-0 px-6 pt-6">
+        <CategoryCard />
+      </div>
+    </div>
+  );
+}
+
+export default MobileMenu;
