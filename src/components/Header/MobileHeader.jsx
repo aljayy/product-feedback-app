7e7db6c3e15edf0a35f075ecf89556cfc20b4181
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-01 15:59:11 -0700

    feat: add functionality to toggle mobile menu

--- src/components/Header/MobileHeader.jsx
+++ src/components/Header/MobileHeader.jsx
@@ -1,8 +1,16 @@
-import React from "react";
-import hamburgerIcon from "../../assets/shared/mobile/icon-hamburger.svg";
+import React, { useState } from "react";
+import close from "../../assets/shared/mobile/icon-close.svg";
+import hamburger from "../../assets/shared/mobile/icon-hamburger.svg";
 import gradient from "../../assets/suggestions/mobile/background-header.png";
+import MobileMenu from "../MobileMenu/MobileMenu";
 
 function MobileHeader() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function toggleMenu() {
+    setShowMenu(!showMenu);
+  }
+
   return (
     <header
       className="bg-no-repeat bg-cover h-[7.2rem] w-full pt-4 pl-6 pb-[1.5rem] pr-[2.587rem] flex justify-between items-center"
@@ -16,9 +24,10 @@ function MobileHeader() {
         </h2>
         <p className="text-white opacity-75 text-s-body">Feedback Board</p>
       </div>
-      <div>
-        <img src={hamburgerIcon} alt="Hamburger Menu Icon" />
+      <div onClick={toggleMenu}>
+        <img src={showMenu ? close : hamburger} alt="Menu Icon" />
       </div>
+      {showMenu && <MobileMenu />}
     </header>
   );
 }
