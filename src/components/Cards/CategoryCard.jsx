1d06ba6a3e5308e52feb62a59a3c57ea73edb3ca
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-01 14:13:29 -0700

    feat: add changeCategory func
    This function will change the selected category from which
    I will sort the feedback requests

--- src/components/Cards/CategoryCard.jsx
+++ src/components/Cards/CategoryCard.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryTag from "../UI/Tags/CategoryTag";
 
-const categories = [
+const initialCategories = [
   { title: "All", selected: true },
   { title: "UI", selected: false },
   { title: "UX", selected: false },
@@ -11,10 +11,28 @@ const categories = [
 ];
 
 function CategoryCard() {
+  const [categories, setCategories] = useState(initialCategories);
+
+  function changeCategory(newCategoryTitle) {
+    setCategories((prevCategories) => {
+      return prevCategories.map((category) => {
+        if (category.title === newCategoryTitle) {
+          return { ...category, selected: true };
+        } else return { ...category, selected: false };
+      });
+    });
+  }
+
   return (
     <div className="bg-white w-full rounded-[1rem] pt-6 pl-6 pr-[1.8rem] pb-9 flex flex-wrap gap-x-[0.8rem] gap-y-[1.4rem]">
       {categories.map((category) => {
-        return <CategoryTag>{category.title}</CategoryTag>;
+        return (
+          <CategoryTag
+            title={category.title}
+            selected={category.selected}
+            onClick={changeCategory}
+          />
+        );
       })}
     </div>
   );
