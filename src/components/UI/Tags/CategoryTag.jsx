69d2278ac2a918ec287b49a025cb2fc892016ab7
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-02 10:17:47 -0700

    fix: add a ternary expression to onClick
    This is done since not every part this component is going
    to be used in will need to supply an onClick func. This fixes
    the bug that pops up when a onClick func is not provided.

--- src/components/UI/Tags/CategoryTag.jsx
+++ src/components/UI/Tags/CategoryTag.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
 function CategoryTag({ title, selected, onClick }) {
+  const capitalizedTitle = title.charAt(0).toUpperCase() + title.slice(1);
+
   return (
     <button
-      onClick={() => onClick(title)}
+      onClick={onClick ? () => onClick(title) : () => {}}
       className={`${
         selected ? "bg-blue text-white" : "bg-grey text-blue hover:bg-tagHover"
       } rounded-[1rem] h-7.5 pt-[0.5rem] pb-1.5 px-4 text-s-body cursor-pointer`}
     >
-      {title}
+      {capitalizedTitle}
     </button>
   );
 }
