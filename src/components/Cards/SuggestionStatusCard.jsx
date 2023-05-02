4b3fd7b7a86f5d23b248984c1a8752db92f12784
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-02 16:09:15 -0700

    feat: dispatch action creator to update redux state

--- src/components/Cards/SuggestionStatusCard.jsx
+++ src/components/Cards/SuggestionStatusCard.jsx
@@ -1,8 +1,12 @@
+import { useDispatch } from "react-redux";
 import UpvoteTag from "../UI/Tags/UpvoteTag";
 import CategoryTag from "../UI/Tags/CategoryTag";
 import CommentCounter from "../UI/CommentCounter/CommentCounter";
+import { toggleUpvote } from "../../store/suggestionSlice";
 
 function SuggestionStatusCard({ request }) {
+  const dispatch = useDispatch();
+
   return (
     <div className="bg-white rounded-[1rem] p-6 grid grid-rows-[1fr_minmax(32px)] grid-cols-2 gap-y-4">
       <div className="col-span-2">
@@ -14,7 +18,10 @@ function SuggestionStatusCard({ request }) {
         </p>
         <CategoryTag title={request.category} />
       </div>
-      <div className="row-start-2">
+      <div
+        className="row-start-2"
+        onClick={() => dispatch(toggleUpvote(request.id))}
+      >
         <UpvoteTag upvoteCount={request.upvotes.count} />
       </div>
       <div className="row-start-2 col-start-2 justify-self-end self-center">
