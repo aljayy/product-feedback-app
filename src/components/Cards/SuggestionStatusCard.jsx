6cad7bb498b63dd5bf07c396d94caaab99d32df3
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-02 16:37:22 -0700

    refactor: move action creators to UpvoteTag component

--- src/components/Cards/SuggestionStatusCard.jsx
+++ src/components/Cards/SuggestionStatusCard.jsx
@@ -1,12 +1,8 @@
-import { useDispatch } from "react-redux";
 import UpvoteTag from "../UI/Tags/UpvoteTag";
 import CategoryTag from "../UI/Tags/CategoryTag";
 import CommentCounter from "../UI/CommentCounter/CommentCounter";
-import { toggleUpvote } from "../../store/suggestionSlice";
 
 function SuggestionStatusCard({ request }) {
-  const dispatch = useDispatch();
-
   return (
     <div className="bg-white rounded-[1rem] p-6 grid grid-rows-[1fr_minmax(32px)] grid-cols-2 gap-y-4">
       <div className="col-span-2">
@@ -18,11 +14,12 @@ function SuggestionStatusCard({ request }) {
         </p>
         <CategoryTag title={request.category} />
       </div>
-      <div
-        className="row-start-2"
-        onClick={() => dispatch(toggleUpvote(request.id))}
-      >
-        <UpvoteTag upvoteCount={request.upvotes.count} />
+      <div className="row-start-2">
+        <UpvoteTag
+          upvoteCount={request.upvotes.count}
+          upvoted={request.upvotes.userUpvoted}
+          requestId={request.id}
+        />
       </div>
       <div className="row-start-2 col-start-2 justify-self-end self-center">
         <CommentCounter
