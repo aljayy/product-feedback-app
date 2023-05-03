4a473efcead73444344ffe044d047cb7033a2981
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-03 14:12:39 -0700

    feat: retrieve state from store

--- src/components/Cards/CategoryCard.jsx
+++ src/components/Cards/CategoryCard.jsx
@@ -1,36 +1,20 @@
-import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { changeSelectedCategory } from "../../store/suggestionSlice";
 import CategoryTag from "../UI/Tags/CategoryTag";
 
-const initialCategories = [
-  { title: "All", selected: true },
-  { title: "UI", selected: false },
-  { title: "UX", selected: false },
-  { title: "Enhancement", selected: false },
-  { title: "Bug", selected: false },
-  { title: "Feature", selected: false },
-];
-
 function CategoryCard() {
-  const [categories, setCategories] = useState(initialCategories);
-
-  function changeCategory(newCategoryTitle) {
-    setCategories((prevCategories) => {
-      return prevCategories.map((category) => {
-        if (category.title === newCategoryTitle) {
-          return { ...category, selected: true };
-        } else return { ...category, selected: false };
-      });
-    });
-  }
+  const categories = useSelector((state) => state.suggestions.categories);
+  const dispatch = useDispatch();
 
   return (
     <div className="bg-white w-full rounded-[1rem] pt-6 pl-6 pr-[1.8rem] pb-9 flex flex-wrap gap-x-[0.8rem] gap-y-[1.4rem]">
-      {categories.map((category) => {
+      {categories.map((category, index) => {
         return (
           <CategoryTag
             title={category.title}
+            key={index}
             selected={category.selected}
-            onClick={changeCategory}
+            onClick={() => dispatch(changeSelectedCategory(category.title))}
           />
         );
       })}
