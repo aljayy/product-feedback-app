6131914f274b0f7492bc879de6931c5a48c55c5c
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-03 11:11:59 -0700

    refactor: retieve added key to state

--- src/components/Suggestions/SuggestionsWrapper.jsx
+++ src/components/Suggestions/SuggestionsWrapper.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import SuggestionStatusCard from "../Cards/SuggestionStatusCard";
 
 function SuggestionsWrapper() {
-  const suggestions = useSelector((state) => state.suggestion);
+  const suggestions = useSelector((state) => state.suggestion.requests);
 
   return (
     <div className="pt-8 px-6 pb-[5.5rem] flex flex-col gap-y-4">
