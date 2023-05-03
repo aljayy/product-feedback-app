91915660365b8cdd1690b786288d1d2c0e2cb11f
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-03 16:51:49 -0700

    feat: add ProductCard component

---
+++ src/components/Cards/ProductCard.jsx
@@ -0,0 +1,15 @@
+import gradient from "../../assets/suggestions/tablet/background-header.png";
+
+function ProductCard() {
+  return (
+    <div
+      className="bg-no-repeat bg-cover w-full rounded-[1rem] text-white flex flex-col justify-end p-6"
+      style={{ backgroundImage: `url(${gradient})` }}
+    >
+      <h2 className="text-l-header">Frontend Mentor</h2>
+      <p className="text-m-body opacity-[.75]">Feedback Board</p>
+    </div>
+  );
+}
+
+export default ProductCard;
