33a238f3915572d982c8b37bf383ca87ede9729b
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-04 15:13:52 -0700

    style: add tablet breakpoint

--- src/components/Suggestions/EmptySuggestions.jsx
+++ src/components/Suggestions/EmptySuggestions.jsx
@@ -3,14 +3,14 @@ import PurpleBtn from "../UI/Buttons/PurpleBtn";
 
 function EmptySuggestions() {
   return (
-    <div className="bg-white rounded-[1rem] py-[7.6rem] px-6 flex flex-col items-center">
-      <div>
-        <img src={empty} alt="Detective Icon" />
+    <div className="bg-white rounded-[1rem] py-[7.6rem] px-6 flex flex-col items-center m:px-[14rem] m:py-[11rem]">
+      <div className="m:h-[13.6rem] m:w-full">
+        <img src={empty} alt="Detective Icon" className="m:mx-auto m:h-full" />
       </div>
-      <h2 className="mt-[3.9rem] text-m-header text-navy">
+      <h2 className="mt-[3.9rem] text-m-header text-navy m:mt-[5.3rem] m:text-xl-header">
         There is no feedback yet.
       </h2>
-      <p className="mt-3.5 mb-6 text-center text-s-body font-normal text-lightNavySecondary ">
+      <p className="mt-3.5 mb-6 text-center text-s-body font-normal text-lightNavySecondary m:mt-4 m:mb-[4.8rem] m:text-l-body">
         Got a suggestion? Found a bug that needs to be squashed? We love hearing
         about new ideas to improve our app.
       </p>
