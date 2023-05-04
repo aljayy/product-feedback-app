c4230ff5acf49f01ec1640acdb6b017a5ba7fbfc
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-04 12:52:23 -0700

    style: add desktop media query

--- src/components/Cards/ProductCard.jsx
+++ src/components/Cards/ProductCard.jsx
@@ -3,7 +3,7 @@ import gradient from "../../assets/suggestions/tablet/background-header.png";
 function ProductCard() {
   return (
     <div
-      className="bg-no-repeat bg-cover w-full rounded-[1rem] text-white flex flex-col justify-end p-6"
+      className="bg-no-repeat bg-cover w-full rounded-[1rem] text-white flex flex-col justify-end p-6 l:pt-[6.2rem]"
       style={{ backgroundImage: `url(${gradient})` }}
     >
       <h2 className="text-l-header">Frontend Mentor</h2>
