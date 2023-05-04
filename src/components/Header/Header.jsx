12c15e819719d21f18b9c7fe5177423b0a6083b9
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-04 14:04:47 -0700

    style: add desktop media query

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import RoadmapCard from "../Cards/RoadmapCard";
 
 function Header() {
   return (
-    <div className="h-[17.8rem] grid grid-cols-3 gap-2.5 mb-10">
+    <div className="h-[17.8rem] grid grid-cols-3 gap-2.5 mb-10 l:grid-cols-none l:w-[25.5rem] l:grid-rows-[auto_auto_auto] l:gap-y-6">
       <ProductCard />
       <CategoryCard />
       <RoadmapCard />
