fe8595bd9ee45c7de32dc75da025fbb379539403
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-04 11:59:44 -0700

    style: add tablet media query

--- src/components/UI/CommentCounter/CommentCounter.jsx
+++ src/components/UI/CommentCounter/CommentCounter.jsx
@@ -3,11 +3,13 @@ import commentIcon from "../../../assets/shared/icon-comments.svg";
 
 function CommentCounter({ count }) {
   return (
-    <div className="flex items-center gap-x-1">
+    <div className="flex items-center gap-x-1 m:gap-x-2">
       <div>
         <img src={commentIcon} alt="Comment Icon" />
       </div>
-      <p className="text-s-body font-bold -tracking-[0.18px]">{count}</p>
+      <p className="text-s-body font-bold -tracking-[0.18px] m:text-l-body m:font-bold m:-tracking-[0.22px]">
+        {count}
+      </p>
     </div>
   );
 }
