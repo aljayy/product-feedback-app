2fc4988f460491b2d9ea75e5ae6359f833c4ae55
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-04 10:00:19 -0700

    style: add tablet media queries

--- src/components/SuggestionBar/SuggestionBar.jsx
+++ src/components/SuggestionBar/SuggestionBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeSort } from "../../store/suggestionSlice";
 import chevron from "../../assets/shared/chevronlight.svg";
+import lightBulb from "../../assets/suggestions/icon-suggestions.svg";
 import PurpleBtn from "../UI/Buttons/PurpleBtn";
 import Dropdown from "../UI/Dropdown/Dropdown";
 
@@ -9,6 +10,9 @@ function SuggestionBar() {
   const [showDropdown, setShowDropdown] = useState(false);
   const dispatch = useDispatch();
   const sorts = useSelector((state) => state.suggestions.sortMethods);
+  const requestCount = useSelector(
+    (state) => state.suggestions.requests.length
+  );
 
   const selectedSort = sorts.find((option) => option.selected);
 
@@ -23,8 +27,14 @@ function SuggestionBar() {
   }
 
   return (
-    <div className="relative bg-navySecondary h-[5.6rem] px-6 py-2 flex items-center justify-between font-main">
-      <div className="text-white text-s-body flex gap-2 items-center">
+    <div className="relative bg-navySecondary h-[5.6rem] px-6 py-2 flex items-center justify-between font-main m:h-[7.2rem] m:rounded-[1rem] m:grid m:grid-cols-[auto_1fr_1fr] m:gap-9.5 m:py-3.5 m:pr-3">
+      <div className="hidden m:flex items-center gap-4">
+        <div>
+          <img src={lightBulb} alt="Light Bulb Icon" />
+        </div>
+        <p className="text-m-header text-white">{`${requestCount} Suggestions`}</p>
+      </div>
+      <div className="text-white text-s-body flex gap-2 items-center m:col-start-2 m:text-s-header m:gap-[0.9rem]">
         <p className="font-normal">
           Sort by :{" "}
           <span className="font-bold" onClick={toggleDropdown}>
@@ -46,7 +56,9 @@ function SuggestionBar() {
           <Dropdown options={sorts} onClick={changeSorting} />
         </div>
       </div>
-      <PurpleBtn>+ Add Feedback</PurpleBtn>
+      <div className="col-start-3 justify-self-end">
+        <PurpleBtn>+ Add Feedback</PurpleBtn>
+      </div>
     </div>
   );
 }
