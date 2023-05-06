9f9318472ed2c57668a223da3521780e68439b35
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-06 11:17:37 -0700

    style: add tablet breakpoint

--- src/components/Roadmap/RoadmapColumn.jsx
+++ src/components/Roadmap/RoadmapColumn.jsx
@@ -3,9 +3,9 @@ import RoadmapStatusCard from "./RoadmapStatusCard";
 
 function RoadmapColumn({ requests, label, desc, colorway }) {
   return (
-    <div className="px-6 pt-6 pb-[9.8rem]">
-      <h2 className="text-m-header text-navy">{`${label} (${requests.length})`}</h2>
-      <p className="text-s-body font-normal text-lightNavySecondary mt-[0.4rem] mb-6">
+    <div className="px-6 pt-6 pb-[9.8rem] m:p-0">
+      <h2 className="text-m-header text-navy m:text-s-header">{`${label} (${requests.length})`}</h2>
+      <p className="text-s-body font-normal text-lightNavySecondary mt-[0.4rem] mb-6 m:text-s-header m:tracking-normal m:font-normal">
         {desc}
       </p>
       {requests.map((request) => {
