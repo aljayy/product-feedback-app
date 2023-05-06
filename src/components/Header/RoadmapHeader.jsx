ecb988139d374f8ae7267375c7d3b651f7a061d2
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-06 12:27:24 -0700

    style: add desktop breakpoint

--- src/components/Header/RoadmapHeader.jsx
+++ src/components/Header/RoadmapHeader.jsx
@@ -4,7 +4,7 @@ import PurpleBtn from "../UI/Buttons/PurpleBtn";
 
 function RoadmapHeader() {
   return (
-    <header className="bg-navySecondary w-full h-[10rem] px-6 py-6.5 flex justify-between items-center m:h-[11.3rem] m:rounded-[1rem] m:py-[2.7rem] m:px-8">
+    <header className="bg-navySecondary w-full h-[10rem] px-6 py-6.5 flex justify-between items-center m:h-[11.3rem] m:rounded-[1rem] m:py-[2.7rem] m:px-8 l:pr-10">
       <div>
         <GoBackBtn light={true} />
         <h2 className="text-m-header text-white mt-[0.3rem] m:text-xl-header m:mt-[0.4rem]">
