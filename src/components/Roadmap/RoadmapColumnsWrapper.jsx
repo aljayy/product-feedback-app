4121fecac5b83cf4472ac7877a19bad883dd18c2
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-06 09:29:24 -0700

    feat: use redux state to position grid

--- src/components/Roadmap/RoadmapColumnsWrapper.jsx
+++ src/components/Roadmap/RoadmapColumnsWrapper.jsx
@@ -3,9 +3,12 @@ import RoadmapColumn from "./RoadmapColumn";
 
 function RoadmapWrapper() {
   const requests = useSelector((state) => state.roadmap.requests);
+  const position = useSelector((state) => state.roadmap.columnPosition);
 
   return (
-    <div className="grid grid-cols-[100vw_100vw_100vw] relative -left-[100vw]">
+    <div
+      className={`grid grid-cols-[100vw_100vw_100vw] relative -left-[${position}]`}
+    >
       <RoadmapColumn
         requests={requests.planned.requests}
         label={"Planned"}
