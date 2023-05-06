610e05669af66d407c8122789423319156729fb5
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-06 12:31:19 -0700

    style: add desktop breakpoint

--- src/pages/RoadmapPage.jsx
+++ src/pages/RoadmapPage.jsx
@@ -4,7 +4,7 @@ import RoadmapColumnsWrapper from "../components/Roadmap/RoadmapColumnsWrapper";
 
 function RoadmapPage() {
   return (
-    <div className="overflow-x-hidden m:pt-[5.6rem] m:px-10">
+    <div className="overflow-x-hidden m:pt-[5.6rem] m:px-10 m:pb-[9.5rem] l:pt-[7.8rem] l:px-[16.5rem] l:pb-[18rem]">
       <RoadmapHeader />
       <StatusSelector />
       <RoadmapColumnsWrapper />
