53906d432fccfa842ff77e26a5a893b98f999800
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-06 11:20:59 -0700

    feat: accept props in order to style component differently on same breakpoint

--- src/components/UI/CommentCounter/CommentCounter.jsx
+++ src/components/UI/CommentCounter/CommentCounter.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import commentIcon from "../../../assets/shared/icon-comments.svg";
 
-function CommentCounter({ count }) {
+function CommentCounter({ count, small }) {
+  const weight = small
+    ? "m:text-s-body m:font-bold m:-tracking-[0.18px]"
+    : "m:text-l-body m:font-bold m:-tracking-[0.22px]";
+
   return (
     <div className="flex items-center gap-x-1 m:gap-x-2">
       <div>
         <img src={commentIcon} alt="Comment Icon" />
       </div>
-      <p className="text-s-body font-bold -tracking-[0.18px] m:text-l-body m:font-bold m:-tracking-[0.22px]">
+      <p className={`text-s-body font-bold -tracking-[0.18px] ${weight}`}>
         {count}
       </p>
     </div>
--- src/components/UI/Tags/UpvoteTag.jsx
+++ src/components/UI/Tags/UpvoteTag.jsx
@@ -3,15 +3,18 @@ import unvotedIcon from "../../../assets/shared/icon-arrow-up.svg";
 import upvotedIcon from "../../../assets/shared/chevronlightup.svg";
 import { toggleUpvote } from "../../../store/suggestionSlice";
 
-function UpvoteTag({ upvoteCount, upvoted, requestId }) {
+function UpvoteTag({ upvoteCount, upvoted, requestId, flex }) {
   const dispatch = useDispatch();
   const icon = upvoted ? upvotedIcon : unvotedIcon;
+  const display = flex
+    ? "m:flex-row gap-x-[0.95rem] py-1.5 pl-4 pr-4"
+    : "m:flex-col gap-y-2 pt-3.5 px-[1.1rem] pb-2";
 
   return (
     <button
       className={`${
         upvoted ? "bg-blue text-white" : "bg-grey text-navy hover:bg-tagHover"
-      } py-1.5 pl-4 pr-[1.3rem] rounded-[1rem] flex items-center gap-x-2.5 m:flex-col m:gap-y-2 m:pt-3.5 m:px-[0.9rem] m:pb-2`}
+      } py-1.5 pl-4 pr-[1.3rem] rounded-[1rem] flex items-center gap-x-2.5 ${display}`}
       onClick={() => dispatch(toggleUpvote(requestId))}
     >
       <div>
