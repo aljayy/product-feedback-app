f18fb2873b4246ecc581cdfae5f6b2a33628a286
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-06 09:49:46 -0700

    style: add tablet media query

--- src/components/Header/RoadmapHeader.jsx
+++ src/components/Header/RoadmapHeader.jsx
@@ -4,10 +4,12 @@ import PurpleBtn from "../UI/Buttons/PurpleBtn";
 
 function RoadmapHeader() {
   return (
-    <header className="bg-navySecondary w-full h-[10rem] px-6 py-6.5 flex justify-between items-center">
+    <header className="bg-navySecondary w-full h-[10rem] px-6 py-6.5 flex justify-between items-center m:h-[11.3rem] m:rounded-[1rem] m:py-[2.7rem] m:px-8">
       <div>
         <GoBackBtn light={true} />
-        <h2 className="text-m-header text-white mt-[3px]">Roadmap</h2>
+        <h2 className="text-m-header text-white mt-[0.3rem] m:text-xl-header m:mt-[0.4rem]">
+          Roadmap
+        </h2>
       </div>
       <PurpleBtn>+ Add Feedback</PurpleBtn>
     </header>
