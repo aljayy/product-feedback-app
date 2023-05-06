015b96b631a9d85365267fe7928519308895d733
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-06 09:30:42 -0700

    feat: set border color based on colorway prop

--- src/components/Roadmap/RoadmapStatusCard.jsx
+++ src/components/Roadmap/RoadmapStatusCard.jsx
@@ -4,9 +4,14 @@ import CommentCounter from "../UI/CommentCounter/CommentCounter";
 import UpvoteTag from "../UI/Tags/UpvoteTag";
 
 function RoadmapStatusCard({ request, colorway, label }) {
+  let borderColor = "";
+  if (colorway === "orange") borderColor = "border-orange";
+  if (colorway === "purple") borderColor = "border-purple";
+  if (colorway === "babyBlue") borderColor = "border-babyBlue";
+
   return (
     <div
-      className={`w-full bg-white px-6 pb-6 pt-4 mb-4 border-t-[0.6rem] border-${colorway} rounded-t-[0.5rem] rounded-b-[1rem]`}
+      className={`w-full bg-white px-6 pb-6 pt-4 mb-4 border-t-[0.6rem] ${borderColor} rounded-t-[0.5rem] rounded-b-[1rem]`}
     >
       <div className="flex items-center gap-2">
         <div className={`h-2 w-2 bg-${colorway} rounded-full`} />
