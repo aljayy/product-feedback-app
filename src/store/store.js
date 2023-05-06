2a6475a3f3e84add1a5030a99fc57d2ceeb8188d
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-05 17:00:37 -0700

    feat: add the roadmapSlice reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import suggestionSlice from "./suggestionSlice";
+import roadmapSlice from "./roadmapSlice";
 
 const store = configureStore({
   reducer: {
     suggestions: suggestionSlice.reducer,
+    roadmap: roadmapSlice.reducer,
   },
 });
 
