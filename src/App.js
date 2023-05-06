6a9416b2bee30e12d9f74d4f6c67f7f18601bd78
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-05 17:00:14 -0700

    feat: create BrowserRouter and implement paths

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
-function App() {}
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SuggestionPage from "./pages/SuggestionPage";
+import RoadmapPage from "./pages/RoadmapPage";
+
+const router = createBrowserRouter([
+  { path: "/", element: <SuggestionPage /> },
+  { path: "/roadmap", element: <RoadmapPage /> },
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
 
 export default App;
