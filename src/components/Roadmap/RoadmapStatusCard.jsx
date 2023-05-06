5bebcf9351f3e3e4abf38464d91f5ce46067590f
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-06 12:29:23 -0700

    style: add desktop breakpoint

--- src/components/Roadmap/RoadmapStatusCard.jsx
+++ src/components/Roadmap/RoadmapStatusCard.jsx
@@ -11,18 +11,18 @@ function RoadmapStatusCard({ request, colorway, label }) {
 
   return (
     <div
-      className={`w-full bg-white px-6 pb-6 pt-4 mb-4 border-t-[0.6rem] last:mb-0 ${borderColor} rounded-t-[0.5rem] rounded-b-[1rem] m:rounded-[0.5rem] m:px-5 m:pt-5`}
+      className={`w-full bg-white px-6 pb-6 pt-4 mb-4 border-t-[0.6rem] last:mb-0 ${borderColor} rounded-t-[0.5rem] rounded-b-[1rem] m:rounded-[0.5rem] m:px-5 m:pt-5 l:mb-6 l:p-8 l:pt-[2.5rem] cursor-pointer group`}
     >
-      <div className="flex items-center gap-2 m:gap-4">
+      <div className="flex items-center gap-x-2 m:gap-x-4">
         <div className={`h-2 w-2 bg-${colorway} rounded-full`} />
-        <p className="text-s-body font-normal text-lightNavySecondary">
+        <p className="text-s-body font-normal text-lightNavySecondary l:text-l-body">
           {label}
         </p>
       </div>
-      <p className="text-s-body text-navy font-bold -tracking-[0.18px] mt-4 mb-[0.9rem] m:mt-3.5">
+      <p className="text-s-body text-navy font-bold -tracking-[0.18px] mt-4 mb-[0.9rem] m:mt-3.5 l:mt-2 l:mb-1 l:text-m-header group-hover:text-blue">
         {request.title}
       </p>
-      <p className="text-s-body font-normal text-lightNavySecondary mb-2 m:mb-6">
+      <p className="text-s-body font-normal text-lightNavySecondary mb-2 m:mb-6 l:text-l-body l:mb-4">
         {request.description}
       </p>
       <CategoryTag title={request.category} />
