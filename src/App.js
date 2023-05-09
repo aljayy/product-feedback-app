9a3a692d034d5a90e33e78be40497d544de463e8
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-09 11:20:48 -0700

    feat: add path for RequestDetailsPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SuggestionPage from "./pages/SuggestionPage";
 import RoadmapPage from "./pages/RoadmapPage";
+import RequestDetailsPage from "./pages/RequestDetailsPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <SuggestionPage /> },
   { path: "/roadmap", element: <RoadmapPage /> },
+  { path: "/request-discussion/:board/:id", element: <RequestDetailsPage /> },
 ]);
 
 function App() {
