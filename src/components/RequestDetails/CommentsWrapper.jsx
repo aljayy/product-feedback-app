90628537588f5a56f8c1b3e4a0b1315412c8ed50
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-10 09:46:56 -0700

    style: add desktop breakpoint

--- src/components/RequestDetails/CommentsWrapper.jsx
+++ src/components/RequestDetails/CommentsWrapper.jsx
@@ -7,9 +7,9 @@ function CommentsWrapper({ comments }) {
   ]).length;
 
   return (
-    <div className="w-full bg-white rounded-[1rem] p-6 m:px-8 m:pb-8">
+    <div className="w-full bg-white rounded-[1rem] p-6 m:px-8 m:pb-8 l:pb-[4.8rem]">
       <h2 className="text-m-header text-navy mb-6 m:mb-7">{`${commentsCount} ${
-        commentsCount === 1 ? "comment" : "comments"
+        commentsCount === 1 ? "Comment" : "Comments"
       }`}</h2>
       <div className="flex flex-col gap-y-6 m:gap-y-8">
         {comments.map((comment) => {
