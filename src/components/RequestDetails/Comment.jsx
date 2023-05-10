6354e1e58cd79ab040830f1250c7aa385ac55f72
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-10 12:18:25 -0700

    refactor: move the replies to a root component to the ReplyComment component
    This is done in order to lean out the Comment component as well
    as to get the ability to manage a separate piece of state
    for each single comment

--- src/components/RequestDetails/Comment.jsx
+++ src/components/RequestDetails/Comment.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useRef, useState } from "react";
+import AddReply from "./AddReply";
+import ReplyComment from "./ReplyComment";
 const regex = /[^/]*$/;
 
 function Comment({ comment }) {
-  let fileName = comment.user.image.match(regex)[0];
   const [barHeight, setBarHeight] = useState(0);
+  const [showReply, setShowReply] = useState(false);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const replyRef = useRef();
+  let fileName = comment.user.image.match(regex)[0];
 
   useEffect(() => {
     function handleResize() {
@@ -34,7 +37,7 @@ function Comment({ comment }) {
 
   return (
     <div className="border-b-[1px] border-b-[#8C92B3]/[0.25] pb-6 last:border-b-0 last:pb-0 m:pb-8">
-      <div className="grid grid-cols-[auto_1fr_1fr] grid-rows-[auto_1fr]">
+      <div className="grid grid-cols-[auto_1fr_1fr] grid-rows-[auto_1fr_auto]">
         <div className="h-10 w-10 rounded-full overflow-hidden">
           <img
             className="object-cover"
@@ -51,7 +54,12 @@ function Comment({ comment }) {
           </p>
         </div>
         <div className="self-center justify-self-end">
-          <button className="text-s-body text-blue">Reply</button>
+          <button
+            className="text-s-body text-blue"
+            onClick={() => setShowReply(!showReply)}
+          >
+            Reply
+          </button>
         </div>
         {comment.replies && (
           <div
@@ -64,6 +72,7 @@ function Comment({ comment }) {
             {comment.content}
           </p>
         </div>
+        {showReply && <AddReply />}
       </div>
       {comment.replies && (
         <div ref={replyRef} className="mt-6 flex gap-x-6 m:ml-5 m:mt-8">
@@ -72,37 +81,8 @@ function Comment({ comment }) {
             style={{ height: `${barHeight}px` }}
           ></div>
           <div className="flex flex-col gap-y-6 m:gap-y-[1.7rem] l:gap-y-8">
-            {comment.replies.map((reply) => {
-              let fileName = reply.user.image.match(regex)[0];
-
-              return (
-                <div className="grid grid-cols-[auto_1fr_1fr] grid-rows-[auto_1fr]">
-                  <div className="h-10 w-10 rounded-full overflow-hidden">
-                    <img
-                      className="object-cover"
-                      src={require(`../../assets/user-images/${fileName}`)}
-                      alt="User Profile Img"
-                    />
-                  </div>
-                  <div className="ml-4 m:ml-8">
-                    <p className="text-s-body text-navy font-bold -tracking-[0.18px] m:text-s-header">
-                      {reply.user.name}
-                    </p>
-                    <p className="text-s-body font-normal text-lightNavySecondary m:text-s-header m:tracking-normal m:font-normal">
-                      {`@${reply.user.username}`}
-                    </p>
-                  </div>
-                  <div className="self-center justify-self-end">
-                    <button className="text-s-body text-blue">Reply</button>
-                  </div>
-                  <div className="row-start-2 col-start-1 col-span-3 mt-4 m:mt-[1rem] m:col-start-2 m:ml-8">
-                    <p className="text-s-body font-normal text-lightNavySecondary m:text-m-body">
-                      <span className="font-bold text-purple">{`@${reply.replyingTo} `}</span>
-                      {reply.content}
-                    </p>
-                  </div>
-                </div>
-              );
+            {comment.replies.map((reply, index) => {
+              return <ReplyComment reply={reply} key={index} />;
             })}
           </div>
         </div>
---
+++ src/components/RequestDetails/ReplyComment.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import AddReply from "./AddReply";
+const regex = /[^/]*$/;
+
+function ReplyComment({ reply }) {
+  let fileName = reply.user.image.match(regex)[0];
+  const [showReply, setShowReply] = useState(false);
+
+  return (
+    <div className="grid grid-cols-[auto_1fr_1fr] grid-rows-[auto_1fr_auto]">
+      <div className="h-10 w-10 rounded-full overflow-hidden">
+        <img
+          className="object-cover"
+          src={require(`../../assets/user-images/${fileName}`)}
+          alt="User Profile Img"
+        />
+      </div>
+      <div className="ml-4 m:ml-8">
+        <p className="text-s-body text-navy font-bold -tracking-[0.18px] m:text-s-header">
+          {reply.user.name}
+        </p>
+        <p className="text-s-body font-normal text-lightNavySecondary m:text-s-header m:tracking-normal m:font-normal">
+          {`@${reply.user.username}`}
+        </p>
+      </div>
+      <div className="self-center justify-self-end">
+        <button
+          className="text-s-body text-blue"
+          onClick={() => setShowReply(!showReply)}
+        >
+          Reply
+        </button>
+      </div>
+      <div className="row-start-2 col-start-1 col-span-3 mt-4 m:mt-[1rem] m:col-start-2 m:ml-8">
+        <p className="text-s-body font-normal text-lightNavySecondary m:text-m-body">
+          <span className="font-bold text-purple">{`@${reply.replyingTo} `}</span>
+          {reply.content}
+        </p>
+      </div>
+      {showReply && <AddReply />}
+    </div>
+  );
+}
+
+export default ReplyComment;
