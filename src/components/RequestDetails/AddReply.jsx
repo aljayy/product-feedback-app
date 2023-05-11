d54d2a7861f15bcff57a8e4fe38560b77d8b6ba4
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-11 14:55:06 -0700

    feat: add handler for posting reply

--- src/components/RequestDetails/AddReply.jsx
+++ src/components/RequestDetails/AddReply.jsx
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addRoadmapReply } from "../../store/roadmapSlice";
+import { addSuggestionReply } from "../../store/suggestionSlice";
 import PurpleBtn from "../UI/Buttons/PurpleBtn";
 
-function AddReply() {
+function AddReply({ requestId, commentId, roadmap, replyingTo }) {
+  const replyRef = useRef();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.general.user);
+
+  function addReplyHandler() {
+    if (roadmap) {
+      dispatch(
+        addRoadmapReply({
+          reply: replyRef.current.value,
+          requestId,
+          commentId,
+          user,
+          replyingTo,
+        })
+      );
+    } else
+      dispatch(
+        addSuggestionReply({
+          reply: replyRef.current.value,
+          requestId,
+          commentId,
+          user,
+          replyingTo,
+        })
+      );
+  }
+
   return (
     <div className="col-start-1 col-span-3 row-start-3 row-end-3 mt-4 flex flex-col items-end">
       <textarea
         className="h-[8rem] w-full bg-lightGrey resize-none mb-4 rounded-[5px] p-4 text-s-body text-navy placeholder:font-normal "
         placeholder="Type your reply here"
+        ref={replyRef}
       ></textarea>
-      <div>
+      <div onClick={addReplyHandler}>
         <PurpleBtn>Post Reply</PurpleBtn>
       </div>
     </div>
