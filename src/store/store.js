f3e998d8b2519e16389ffea11803ac1da17d5d5a
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-11 15:00:27 -0700

    feat: add general reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import suggestionSlice from "./suggestionSlice";
 import roadmapSlice from "./roadmapSlice";
+import generalSlice from "./generalSlice";
 
 const store = configureStore({
   reducer: {
     suggestions: suggestionSlice.reducer,
     roadmap: roadmapSlice.reducer,
+    general: generalSlice.reducer,
   },
 });
 
