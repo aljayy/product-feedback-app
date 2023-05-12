2ad390c25f36dc08f8d0865b8ef1d8dccc8ca6f3
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-12 11:32:55 -0700

    style: add hover styles

--- src/components/Suggestions/SuggestionStatusCard.jsx
+++ src/components/Suggestions/SuggestionStatusCard.jsx
@@ -11,9 +11,9 @@ function SuggestionStatusCard({ request }) {
 
   return (
     <Link to={`/request-discussion/suggestions/${request.id}`}>
-      <div className="bg-white rounded-[1rem] p-6 grid grid-rows-[1fr_minmax(32px)] grid-cols-2 gap-y-4 m:py-7 m:px-8 m:grid-rows-none m:grid-cols-[auto_1fr_auto] cursor-pointer">
+      <div className="bg-white rounded-[1rem] p-6 grid grid-rows-[1fr_minmax(32px)] grid-cols-2 gap-y-4 m:py-7 m:px-8 m:grid-rows-none m:grid-cols-[auto_1fr_auto] cursor-pointer group">
         <div className="col-span-2 m:row-start-1 m:col-start-2 m:col-span-1">
-          <h2 className="text-s-body font-bold text-navy -tracking-[0.18px] m:text-m-header">
+          <h2 className="text-s-body font-bold text-navy -tracking-[0.18px] m:text-m-header group-hover:text-blue">
             {request.title}
           </h2>
           <p className="text-s-body font-normal text-lightNavySecondary my-[0.9rem] m:text-l-body m:mt-1 m:mb-3">
