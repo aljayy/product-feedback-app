55c431316cdf95136ade76f6e62f0687ccdb891b
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-15 14:23:18 -0700

    feat: add EditRequestPage to router

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SuggestionPage from "./pages/SuggestionPage";
 import RoadmapPage from "./pages/RoadmapPage";
 import RequestDetailsPage from "./pages/RequestDetailsPage";
+import EditRequestPage from "./pages/EditRequestPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <SuggestionPage /> },
   { path: "/roadmap", element: <RoadmapPage /> },
   { path: "/request-discussion/:board/:id", element: <RequestDetailsPage /> },
+  { path: "/edit-request/:board/:id", element: <EditRequestPage /> },
 ]);
 
 function App() {
