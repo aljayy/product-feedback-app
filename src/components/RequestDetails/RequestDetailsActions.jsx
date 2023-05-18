22c2ec3cad68498d462692f0dee8d2234000143c
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-17 17:40:05 -0700

    feat: add Link component to navigate user

--- src/components/RequestDetails/RequestDetailsActions.jsx
+++ src/components/RequestDetails/RequestDetailsActions.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import GoBackBtn from "../UI/Buttons/GoBackBtn";
 import BlueBtn from "../UI/Buttons/BlueBtn";
+import { Link } from "react-router-dom";
 
-function RequestDetailsActions() {
+function RequestDetailsActions({ board, id }) {
+  console.log(board, id);
+
   return (
     <div className="flex justify-between items-center">
       <GoBackBtn light={false} />
-      <BlueBtn>Edit Feedback</BlueBtn>
+      <Link to={`/edit-request/${board}/${id}`}>
+        <BlueBtn>Edit Feedback</BlueBtn>
+      </Link>
     </div>
   );
 }
