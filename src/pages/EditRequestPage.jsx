8c332270fb1292a7bbf6edca0232c43ad875f0cb
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-18 15:59:32 -0700

    feat: add EditRequestPage

---
+++ src/pages/EditRequestPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EditRequest from "../components/EditRequest/EditRequest";
+
+function EditRequestPage() {
+  return (
+    <div>
+      <EditRequest />
+    </div>
+  );
+}
+
+export default EditRequestPage;
