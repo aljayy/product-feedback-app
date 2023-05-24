8b7a3d08d39de1ffd63a79fbce88e7f56f08c4a4
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-24 13:39:57 -0700

    feat: add AddRequestPage component

---
+++ src/pages/AddRequestPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RequestForm from "../components/UI/Form/RequestForm";
+
+function AddRequestPage() {
+  return (
+    <div>
+      <RequestForm />
+    </div>
+  );
+}
+
+export default AddRequestPage;
