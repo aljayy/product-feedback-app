35b6fb47a3dbd53159621113e19d0f72bc33f32b
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-23 18:13:59 -0700

    feat: add state to check whether form should be in new or editing mode

--- src/components/UI/Form/RequestForm.jsx
+++ src/components/UI/Form/RequestForm.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import editIcon from "../../../assets/shared/icon-edit-feedback.svg";
+import newIcon from "../../../assets/shared/icon-new-feedback.svg";
 
 function RequestForm() {
-  return <div>RequestForm</div>;
+  const [editing, setEditing] = useState(false);
+  const { board, id } = useParams();
+
+  useEffect(() => {
+    if (board && id) setEditing(true);
+    else setEditing(false);
+  }, [board, id]);
+
+  const request = useSelector((state) => {
+    let temp;
+    if (board === "suggestions") {
+      return (temp = state.suggestions.requests.find(
+        (request) => request.id === Number(id)
+      ));
+    } else {
+      for (const key in state.roadmap.requests) {
+        state.roadmap.requests[key].requests.forEach((request) => {
+          if (request.id === Number(id)) {
+            return (temp = request);
+          }
+        });
+      }
+    }
+    return temp;
+  });
+
+  console.log(request);
+
+  useEffect(() => {
+    if (board && id) setEditing(true);
+    else setEditing(false);
+  }, [board, id]);
+
+  const icon = editing ? editIcon : newIcon;
+
+  return (
+    <form className="bg-white rounded-[1rem] w-full">
+      <img src={icon} alt="Form Icon" />
+      <h2></h2>
+    </form>
+  );
 }
 
 export default RequestForm;
