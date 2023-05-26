57a33fa728bb6fdd993571c6ddbde4255ff81c79
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-05-25 18:32:04 -0700

    feat: add AddRequestPage route

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import SuggestionPage from "./pages/SuggestionPage";
 import RoadmapPage from "./pages/RoadmapPage";
 import RequestDetailsPage from "./pages/RequestDetailsPage";
 import EditRequestPage from "./pages/EditRequestPage";
+import AddRequestPage from "./pages/AddRequestPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <SuggestionPage /> },
   { path: "/roadmap", element: <RoadmapPage /> },
   { path: "/request-discussion/:board/:id", element: <RequestDetailsPage /> },
   { path: "/edit-request/:board/:id", element: <EditRequestPage /> },
+  { path: "/new-request", element: <AddRequestPage /> },
 ]);
 
 function App() {
