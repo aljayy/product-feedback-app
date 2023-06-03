c299318e6a5305fd9e80c2efe0efa095bc153343
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2023-06-02 17:13:48 -0700

    feat: add GoBackBtn component to AddRequestPage

--- src/pages/AddRequestPage.jsx
+++ src/pages/AddRequestPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import GoBackBtn from "../components/UI/Buttons/GoBackBtn";
 import RequestForm from "../components/UI/Form/RequestForm";
 
 function AddRequestPage() {
   return (
     <div>
+      <GoBackBtn light={false} />
       <RequestForm />
     </div>
   );
